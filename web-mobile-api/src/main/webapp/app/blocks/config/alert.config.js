1518f523d07ab371376b9a1f1227552afc19df11
Duy Chung <duy.chung@formos.com>
2017-01-11 13:17:52 +0700

    init again

---
+++ web-mobile-api/src/main/webapp/app/blocks/config/alert.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('webmobileapiApp')
+        .config(alertServiceConfig);
+
+    alertServiceConfig.$inject = ['AlertServiceProvider'];
+
+    function alertServiceConfig(AlertServiceProvider) {
+        // set below to true to make alerts look like toast
+        AlertServiceProvider.showAsToast(false);
+    }
+})();
